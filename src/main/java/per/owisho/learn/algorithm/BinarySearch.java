f714037ac44668b04ef4e98b450c298277443299
owisho <owisho2012@126.com>
2018-10-28 23:06:57 +0800

    二分查找

--- src/main/java/per/owisho/learn/algorithm/BinarySearch.java
+++ src/main/java/per/owisho/learn/algorithm/BinarySearch.java
@@ -17,19 +17,14 @@ public class BinarySearch {
     public static int binarySearch(int[] arrs,int obj) {
         int low = 0;
         int high = arrs.length-1;
-        if(arrs[low]==obj){
-            return low;
-        }else if(arrs[high]==obj){
-            return high;
-        }
         while(low<=high){
             int mid = (low+high)/2;
             if(arrs[mid]==obj){
                 return mid;
             }else if(arrs[mid]<obj){
-                low = mid;
+                low = mid+1;
             }else if(arrs[mid]>obj){
-                high = mid;
+                high = mid-1;
             }
         }
         return -1;
@@ -37,7 +32,7 @@ public class BinarySearch {
 
     public static void main(String[] args) {
         int[] arrs = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
-        System.out.println(binarySearch(arrs,16));
+        System.out.println(binarySearch(arrs,1));
     }
 
 }
